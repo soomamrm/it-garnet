b94388ce22e76393968fce1aa7eee1687dd82c6c
soomamrm <145705212+soomamrm@users.noreply.github.com>
2024-03-04 00:04:32 +0000

    updated slick demo

--- slick-demo/assets/js/scripts.js
+++ slick-demo/assets/js/scripts.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
 
-    $('.fade').slick({
-      dots: true,
-      infinite: true,
-      speed: 500,
-      fade: true,
-      slide: 'div',
-      cssEase: 'linear',
-      autoplay: true,
-      autoplaySpeed: 2000
-    });
-
+  $('.fade').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    slidesToShow: 1, // Show one slide at a time
+    slidesToScroll: 1, // Scroll one slide at a time
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
 
 });
